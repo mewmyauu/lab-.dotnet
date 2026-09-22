181ee4d4b68096ee9ba3f57c38f5ce37d587ed07
mewmyauu <rk.vladislav@gmail.com>
2026-09-22 13:26:00 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("privet ot isp-243!");
-Console.WriteLine("avtor: r. vladislav");
-Console.WriteLine("god: 2026");
+int age = 20;
+double gpa1 = 4.5;
+bool isstudent = true;
+string name = "ivan";
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa1);
+Console.WriteLine(isstudent);
+
+var city = "Москва";
+var year = 2026;
+var pi = 3.14;
+var isactive = true;
+
+string myname = "vlad";
+int myage = 20;
+string mygroup = "isp-243";
+Console.WriteLine($"menya zovut {myname}, mne {myage} let, ya uchus v gruppe {mygroup}");
+
+Console.Write("vvedite vashe imya: ");
+string name2 = Console.ReadLine();
+Console.WriteLine($"Привет, {name2}");
+
+Console.Write("Vvedite vash vozrast: ");
+string input = Console.ReadLine();
+int age2 = int.Parse(input);
+Console.WriteLine($"cherez 10 let vam budet {age2 + 10} let.");
+
+int x = 10;
+int y = 3;
+
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
+Console.WriteLine(x % y);
+
+string firstname = "vlad";
+string lastname = "r";
+string group = "isp-243";
+int birthyear = 2006;
+double gpa = 4.5;
+bool hasscholarship = true;
+
+int currentyear = 2026;
+int hisage = currentyear - birthyear;
+
+Console.WriteLine("stud. udostoverenie");
+Console.WriteLine($"imya: {firstname} {lastname}");
+Console.WriteLine($"gruppa: {group}");
+Console.WriteLine($"vozrast: {age} let");
+Console.WriteLine($"srednit ball: {gpa}");
+Console.WriteLine($"spipendiya: {hasscholarship}");
+
+Console.Write("\nvvedite vash lubimiy predmet: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"otlichno. {firstname} lubit {subject}");
+
+
+
+
+
 
