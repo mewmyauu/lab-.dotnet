9c5819e9068caa0839dd9fa1cf79222fbede00e3
mewmyauu <rk.vladislav@gmail.com>
2026-09-22 12:51:37 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("privet ot isp-243!");
+Console.WriteLine("avtor: r. vladislav");
+Console.WriteLine("god: 2026");
+
